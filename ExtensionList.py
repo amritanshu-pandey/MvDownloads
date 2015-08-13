d9af3c0777a37521a75ad92a69c9acb6bd6bded6
Amritanshu Pandey <amp.msit@gmail.com>
2015-08-13 21:42:57 +1000

    Initial Version

---
+++ ExtensionList.py
@@ -0,0 +1,80 @@
+###
+# Lists of popular file extensions
+###
+installerList = [
+    "deb",
+    "run",
+    "exe",
+    "rpm"
+]
+
+pdfList = [
+    "pdf",
+    "xps",
+]
+
+musicList = [
+    "mp3",
+    "aac",
+    "wma",
+    "ogg"
+]
+
+videoList = [
+    "wmv",
+    "mp4",
+    "flv"
+]
+
+ebookList = [
+    "epub",
+    "awz3",
+    "mobi"
+]
+
+docList = [
+    "doc",
+    "docx",
+    "odt",
+    "ppt",
+    "pptx",
+    "xls",
+    "xlsx",
+    "ods"
+]
+
+picList = [
+    "jpg",
+    "jpeg",
+    "gif",
+    "png",
+    "bmp",
+    "raw"
+]
+
+imageList = [
+    "iso",
+    "dmg",
+    "img"
+]
+
+archiveList = [
+    "tar",
+    "zip",
+    "tar.gz",
+    "7z",
+    "rar",
+    "tgz"
+]
+
+scriptList = [
+    "sh",
+    "py",
+    "bash",
+    "shell"
+]
+
+TBDList = [
+    "ica",
+    "torrent"
+]
--- MvD.py
+++ MvD.py
@@ -1,71 +1,44 @@
-import os, glob
+import os, glob, shutil
+
+from ExtensionList import *
 
 ###
 # Setup the source and Destination directories
 ###
-downloadsDir = "~/Downloads"
-musicDir = "~/Music"
-videoDir = "~/Videos"
-pdfDir = "~/Documents/PDFs"
-ebookDir = "~/Documents/Ebooks"
-docDir = "~/Documents"
-installerDir = "~/DEBs"
 
-###
-# Lists of popular file extensions
-###
-installerList = [
-    "deb",
-    "run",
-    "exe",
-    "rpm"
-]
 
-pdfList = [
-    "pdf",
-    "xps",
-]
+directoryList = {
+    "downloadsDir":"~/Downloads",
+    "musicDir":"~/Music",
+    "videoDir":"~/Videos",
+    "pdfDir":"~/Documents/PDFs",
+    "ebookDir":"~/Documents/Ebooks",
+    "docDir":"~/Documents",
+    "installerDir":"~/DEBs",
+    "picDir":"~/Pictures",
+    "imageDir":"~/ISOs",
+    "archiveDir":"~/Downloads/Archives",
+    "scriptDir":"~/Downloads/Scripts",
+    "TBDDir":"~/Downloads/TBD"
 
-musicList = [
-    "mp3",
-    "aac",
-    "wma",
-    "ogg"
-]
-
-videoList = [
-    "wmv",
-    "mp4",
-    "flv"
-]
-
-ebookList = [
-    "epub",
-    "awz3",
-    "mobi"
-]
-
-docList = [
-    "doc",
-    "docx",
-    "odt",
-    "ppt",
-    "pptx",
-    "xls",
-    "xlsx"
-]
+}
 
 ###
 # List of files to run this program for
 ###
 
 runForList = {
-    "installerFiles":[i for i in installerList],
-    "pdfFiles":[i for i in pdfList],
-    "musicFiles":[i for i in musicList],
-    "videoFiles":[i for i in videoList],
-    "ebookFiles":[i for i in ebookList],
-    "docFiles":[i for i in docList]
+    "installerFiles":[[i for i in installerList],directoryList["installerDir"]],
+    "pdfFiles":[[i for i in pdfList],directoryList["pdfDir"]],
+    "musicFiles":[[i for i in musicList],directoryList["musicDir"]],
+    "videoFiles":[[i for i in videoList],directoryList["videoDir"]],
+    "ebookFiles":[[i for i in ebookList],directoryList["ebookDir"]],
+    "docFiles":[[i for i in docList],directoryList["docDir"]],
+    "picFiles":[[i for i in picList],directoryList["picDir"]],
+    "imageFiles":[[i for i in imageList],directoryList["imageDir"]],
+    "archiveFiles": [[i for i in archiveList],directoryList["archiveDir"]],
+    "scriptFiles": [[i for i in scriptList],directoryList["scriptDir"]],
+    "TBDFiles": [[i for i in TBDList],directoryList["TBDDir"]]
 }
 
 ###
@@ -93,11 +66,10 @@ def checkAndCreateDir(dirName):
         print("\tCreating directory " + os.path.join(os.path.expanduser(dirName)))
         try:
             os.makedirs(os.path.join(os.path.expanduser(dirName)))
+            print("\tDirectory created")
         except:
             print("\tUnable to create the directory")
-        finally:
-            print("\tDirectory created")
-        printUnderlines("*",len(os.path.expanduser(downloadsDir)) + 25)
+        printUnderlines("*",len(os.path.expanduser(directoryList["downloadsDir"])) + 25)
 
 ###
 # Print files from the Staging Dictionary
@@ -111,35 +83,52 @@ def printFileNames():
                 print(files)
             print("\n")
 
+def moveFiles(dirName):
+    for keys in stagingDict.keys():
+        if(len(stagingDict[keys])>0):
+            for files in stagingDict[keys]:
+                try:
+                    shutil.move(files,os.path.expanduser(dirName))
+                    print("File "+files+" moved from "+directoryList["downloadsDir"]+" to "+os.path.expanduser(dirName))
+                except:
+                    print("Unable to move file "+files+" from "+directoryList["downloadsDir"]+" to "+os.path.expanduser(dirName))
+            print("\n")
+
 
 ###
 # Print various Directories
 ###
-printCwd()
-print("\n")
-print('Home directory: ' + os.path.expanduser('~'))
-print("Downloads Directory: " + os.path.join(os.path.expanduser(downloadsDir)))
-print("Music Directory: " + os.path.join(os.path.expanduser(musicDir)))
-checkAndCreateDir(musicDir)
-print("Videos Directory: " + os.path.join(os.path.expanduser(videoDir)))
-checkAndCreateDir(videoDir)
-print("PDFs Directory: " + os.path.join(os.path.expanduser(pdfDir)))
-checkAndCreateDir(pdfDir)
-print("EBooks Directory: " + os.path.join(os.path.expanduser(ebookDir)))
-checkAndCreateDir(ebookDir)
-print("Documents Directory: " + os.path.join(os.path.expanduser(docDir)))
-checkAndCreateDir(docDir)
-print("Installers Directory: " + os.path.join(os.path.expanduser(installerDir)))
-checkAndCreateDir(installerDir)
-print("\n")
+# printCwd()
+# print("\n")
+# print('Home directory: ' + os.path.expanduser('~'))
+# print("Downloads Directory: " + os.path.join(os.path.expanduser(downloadsDir)))
+# print("Music Directory: " + os.path.join(os.path.expanduser(musicDir)))
+# checkAndCreateDir(musicDir)
+# print("Videos Directory: " + os.path.join(os.path.expanduser(videoDir)))
+# checkAndCreateDir(videoDir)
+# print("PDFs Directory: " + os.path.join(os.path.expanduser(pdfDir)))
+# checkAndCreateDir(pdfDir)
+# print("EBooks Directory: " + os.path.join(os.path.expanduser(ebookDir)))
+# checkAndCreateDir(ebookDir)
+# print("Documents Directory: " + os.path.join(os.path.expanduser(docDir)))
+# checkAndCreateDir(docDir)
+# print("Installers Directory: " + os.path.join(os.path.expanduser(installerDir)))
+# checkAndCreateDir(installerDir)
+# print("\n")
+
+def printDirs():
+    for dirType in directoryList.keys():
+        print(dirType+": \t"+directoryList[dirType])
+        checkAndCreateDir(directoryList[dirType])
 
 ###
 # Switch to the Downloads directory
 ###
-os.chdir(os.path.join(os.path.expanduser(downloadsDir)))
+os.chdir(os.path.join(os.path.expanduser(directoryList["downloadsDir"])))
 printCwd()
 print("\n")
-
+printDirs()
+print("\n")
 
 for fileType in runForList.keys():
     ###
@@ -149,65 +138,7 @@ for fileType in runForList.keys():
     ###
     # Prepare lists of different installer files
     ###
-    for fileType in runForList[fileType]:
-        stagingDict[fileType] = glob.glob("*." + fileType)
+    for fileTypes in runForList[fileType][0]:
+        stagingDict[fileTypes] = glob.glob("*." + fileTypes)
     printFileNames()
-
-
-# ###
-# # Temporary dictionary to store the file names
-# ###
-# stagingDict = {}
-# ###
-# # Prepare lists of different PDF/XPS files
-# ###
-# for pdfFiles in pdfList:
-#     stagingDict[pdfFiles] = glob.glob("*." + pdfFiles)
-# printFileNames()
-#
-#
-# ###
-# # Temporary dictionary to store the file names
-# ###
-# stagingDict = {}
-# ###
-# # Prepare lists of different PDF/XPS files
-# ###
-# for videoFiles in videoList:
-#     stagingDict[videoFiles] = glob.glob("*." + videoFiles)
-# printFileNames()
-#
-#
-# ###
-# # Temporary dictionary to store the file names
-# ###
-# stagingDict = {}
-# ###
-# # Prepare lists of different PDF/XPS files
-# ###
-# for ebookFiles in ebookList:
-#     stagingDict[ebookFiles] = glob.glob("*." + ebookFiles)
-# printFileNames()
-#
-# ###
-# # Temporary dictionary to store the file names
-# ###
-# stagingDict = {}
-# ###
-# # Prepare lists of different PDF/XPS files
-# ###
-# for musicFiles in musicList:
-#     stagingDict[musicFiles] = glob.glob("*." + musicFiles)
-# printFileNames()
-#
-# ###
-# # Temporary dictionary to store the file names
-# ###
-# stagingDict = {}
-# ###
-# # Prepare lists of different PDF/XPS files
-# ###
-# for docFiles in docList:
-#     stagingDict[docFiles] = glob.glob("*." + docFiles)
-# printFileNames()
-
+    moveFiles(runForList[fileType][1])
